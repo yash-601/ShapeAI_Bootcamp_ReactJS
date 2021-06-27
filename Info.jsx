1bc4f9f7743e4f6c95d22354eea8e14535351120
yash-601 <68612868+yash-601@users.noreply.github.com>
2021-06-27 19:14:54 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Info() {
+    return(
+        <div className="note">
+            <h1>Javascript and ReactJS</h1>
+            <p>A Basic Webdev ReactJS Bootcamp</p>
+        </div>
+    );
+}
+
+export default Info;
